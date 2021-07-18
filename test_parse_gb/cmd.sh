f2043a44a51fbba0ee9873b9812f838cf6c50706
longfei.fu <longfei.fu@thermofisher.com>
2021-07-18 23:49:45 +0800

    update

--- test_parse_gb/cmd.sh
+++ test_parse_gb/cmd.sh
@@ -1,2 +1,3 @@
-python3 t.py >AB981583.gb.feature.type.source.log
-python3 ../parse_gb_file.py -gb AB981583.gb -of hehe
+#python3 t.py >AB981583.gb.feature.type.source.log
+#cat AB981583.gb NC_005816.gb >combine.two.items.gb
+python3 ../parse_gb_file.v2.py -gb combine.two.items.gb -of hehe
